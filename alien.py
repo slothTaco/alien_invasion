6bd2c08ecae2939eca099c8fe215b8e7765eb823
slothTaco <pickeringwilliamscott@gmail.com>
2025-02-28 10:39:57 -0600

    Update alien.py
    check for edges of the screen
    move alien right or left

--- alien.py
+++ alien.py
@@ -1,21 +1,34 @@
 import pygame
+
 from pygame.sprite import Sprite
+
 
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
-    def __init__(Self, ai_game):
+    def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
-        Self.screen = ai_game.screen
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
-        Self.image = pygame.image.load('images/alien.bmp')
-        Self.rect = Self.image.get_rect()
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
 
-        # Start eah new alien near the top left screen.
-        Self.rect.x = Self.rect.width
-        Self.rect.y = Self.rect.height
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
 
-        #Store teh alien's exct horizontal position.
-        Self.x = float(Self.rect.x)
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
